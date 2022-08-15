9a61f5a93e9b2340059c9d2a67e434d499fbbcf0
binayaksadangi <youbinayak@gmail.com>
2022-08-16 05:26:24 +0530

    user model

--- controller/authController.js
+++ controller/authController.js
@@ -1,13 +1,22 @@
 //signup_get, signup_post, login_get, login_post
+const User = require('../models/users')
 
 const signup_get = (req,res)=>{
     res.render('signup');
 }
 
-const signup_post = (req,res)=>{
+const signup_post = async (req,res)=>{
     const {email,password} = req.body;
-    console.log(email,password);
-    res.send("signed in");
+    
+    try{
+        //create a new user
+        const user = await User.create({email,password});
+        res.status(200).json(user);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(404).send('error user not created');
+    }
 }
 
 const login_get = (req,res)=>{
