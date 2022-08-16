79c7014b5245f8b99aed1746f72e6d230c79add2
binayaksadangi <youbinayak@gmail.com>
2022-08-16 06:06:34 +0530

    validation added

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,28 @@
 //signup_get, signup_post, login_get, login_post
 const User = require('../models/users')
 
+//handel Errors
+const handelError = (err)=>{
+    console.log(err.message,err.code);
+    let errors = { email: '', password:''};
+
+    //duplicate error code
+    if(err.code === 11000) {
+        errors.email = 'that email is already registered'
+    }
+
+    //validation error
+    if(err.message.includes('user validation failed')) {
+        //we can't make it this way bcz every time we are going to get both the errors so sometimes it may crash the application
+        // errors.email = err.errors.email.message;
+        // errors.password = err.errors.password.message;
+        Object.values(err.errors).forEach(({properties})=>{
+            errors[properties.path] = properties.message;
+        })
+    }
+    return errors;
+}
+
 const signup_get = (req,res)=>{
     res.render('signup');
 }
@@ -14,8 +36,8 @@ const signup_post = async (req,res)=>{
         res.status(200).json(user);
     }
     catch (err) {
-        console.log(err);
-        res.status(404).send('error user not created');
+        const errors = handelError(err);
+        res.status(404).json({errors});
     }
 }
 
--- models/users.js
+++ models/users.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
+//to validate an email we use this validator
+const {isEmail} = require('validator');
 
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
-        required: true,
+        required: [true,"Please enter an Email"],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail,'Please enter a valid Email']
     },
     password:{
         type: String,
-        required: true,
-        minLength: 6
+        required: [true,"Please enter a password"],
+        minlength: [6,"Password must be 6 characters long"]
     }
 })
 
